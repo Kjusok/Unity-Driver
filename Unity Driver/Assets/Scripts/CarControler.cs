34f058012f2d8391bb43fab8f193401252cc7af2
Kjusok <103447181+Kjusok@users.noreply.github.com>
2022-10-22 20:15:09 +0300

    Car control implemented. Added track and obstacles
    Car control implemented. Added track and obstacles

---
+++ Unity Driver/Assets/Scripts/CarControler.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+
+public class CarControler : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRotation;
+    [SerializeField] private Camera _camera;
+    [SerializeField] [Range(0, 1)] private float _helpValue;
+
+    private Rigidbody _rb;
+    private float _lastRotationY;
+    private Vector3 _movementVector;
+    public bool _onGround;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        _onGround = false;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        _onGround = true;
+    }
+
+    private void SteerHelpAssist()
+    {
+        if (!_onGround)
+        {
+            return;
+        }
+
+        if (Mathf.Abs(transform.rotation.eulerAngles.y - _lastRotationY) < 10f)
+        {
+            float turnAdjust = (transform.rotation.eulerAngles.y - _lastRotationY) * _helpValue;
+            Quaternion rotateHelp = Quaternion.AngleAxis(turnAdjust, Vector3.up);
+            _rb.velocity = rotateHelp * _rb.velocity;
+        }
+
+        _lastRotationY = transform.rotation.eulerAngles.y;
+    }
+
+    private void CreateVectorForce()
+    {
+        var vertical = Input.GetAxis("Vertical");
+
+        float angelY = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+        float cos = Mathf.Cos(angelY);
+        float sin = Mathf.Sin(angelY);
+
+        _movementVector = new Vector3(vertical * sin, 0.0f, vertical * cos);
+    }
+
+    private void Rotation()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (_rb.velocity.sqrMagnitude > 1 && _onGround)
+        {
+            transform.Rotate(0.0f, horizontal * _speedRotation, 0.0f, Space.World);
+        }
+    }
+
+    private void AplyForceToMovementCar()
+    {
+        if (_onGround)
+        {
+            _rb.AddForce(_movementVector * _speed, ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CreateVectorForce();
+        Rotation();
+        AplyForceToMovementCar();
+        SteerHelpAssist();
+    }
+}
